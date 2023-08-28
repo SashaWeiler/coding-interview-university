42eb66cad5a574d678d5a18d4a6cad92d61a3bfe
Sasha Weiler <sasha.p.weiler@gmail.com>
2023-08-28 10:40:36 -0400

    Data Structures

---
+++ CS50/Lecture 3 - Algorithms/numbers.py
@@ -0,0 +1,11 @@
+numbers = [4,6,8,2,7,5,0]
+
+guess = 0
+
+def bin_search(arr, guess):
+    for i in arr:
+        if i == guess:
+            print("Found")
+            return i
+        elif i > 
+        
